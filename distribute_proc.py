c6967bb2828881b90daceee8884f1387257cc309
Soham Sinha <sohamcodes@sohamcodes.cam.corp.google.com>
2017-06-01 16:16:04 -0400

    corrected config for envoy with increased timeout, some changes in other configs as well

--- distribute_proc.py
+++ distribute_proc.py
@@ -1,16 +1,29 @@
 #this sets the nginx processes into 5 cores
 #envoy into 5 cores
-#h2load will be set to 5 cores for 10 clients
+#h2load will be set to 5 cores for 100 clients
 import sh, io, sys, shlex
 
-nginx_start_core = 0
-nginx_end_core = 4
-envoy_start_core = 5
-envoy_end_core = 9
-h2load_start_core = 10
-h2load_end_core = 14
+write_result = False
 
-#allocate nginx to 5 cores
+def process_h2load_output(line):
+    global write_result
+    if write_result:
+        open("result.txt", "a+").write(line)
+    if line.strip() == "progress: 100% done":
+        write_result = True
+
+nginx_start_core = 0
+nginx_end_core = 9
+envoy_start_core = 10
+envoy_end_core = 14
+h2load_start_core = 15
+h2load_end_core = 19
+
+#arguments to program
+envoy_path = sys.argv[1]
+envoy_config_path = sys.argv[2]
+
+#allocate nginx to designated cores
 buf = io.StringIO()
 sh.pgrep('nginx', _out=buf)
 for x in buf.getvalue().split('\n'):
@@ -18,23 +31,27 @@ for x in buf.getvalue().split('\n'):
     if(x.strip() != ''):
         sh.sudo.taskset('-cp', str(nginx_start_core) + '-' + str(nginx_end_core), x, _out=output)
 
-#allocate envoy to 5 cores
+#allocate envoy to designated cores
 #./envoy-fastbuild -c envoy-configs/simple-loopback.json -l debug > out.txt 2>&1 &
-envoyconfig = "-c envoy-configs/simple-loopback.json -l debug"
+envoyconfig = "-c " + envoy_config_path + " -l debug"
 outfile = "out.txt"
-envoy = sh.Command("./envoy-fastbuild")
+envoy = sh.Command(envoy_path)
 #this creates the process in the background, however it'll be destroyed once the python script is finished
 #if we really need envoy to keep running on background after exiting python script, then we probably should use subprocess instead of sh
 run = envoy(envoyconfig.split(" "), _out = outfile, _err_to_out = True, _bg = True)
 print("envoy process id is: " + str(run.pid))
 sh.sudo.taskset('-cp', str(envoy_start_core) + '-' + str(envoy_end_core), str(run.pid), _out=output)
 
-#allocate h2load to 5 cores
-
-h2load_args = "-c " + str(h2load_start_core) + '-' + str(h2load_end_core) + " h2load https://localhost -n10000 -c100 -m10 -t10"
-sh.sudo.taskset(h2load_args.split(' '), _out = "res_direct.txt")
+#allocate h2load to designated cores
+open("result.txt", "w").write("")
+write_result = False
+h2load_args = "-c " + str(h2load_start_core) + '-' + str(h2load_end_core) + " h2load https://localhost -n100000 -c100 -m10 -t5"
+sh.sudo.taskset(h2load_args.split(' '), _out = process_h2load_output)
 print("h2load direct is done.")
 
-h2load_args = "-c " + str(h2load_start_core) + '-' + str(h2load_end_core) + " h2load https://localhost:9000 -n10000 -c100 -m10 -t10"
-sh.sudo.taskset(h2load_args.split(' '), _out = "res_envoy.txt")
+write_result = False
+h2load_args = "-c " + str(h2load_start_core) + '-' + str(h2load_end_core) + " h2load https://localhost:9000 -n100000 -c100 -m10 -t5"
+sh.sudo.taskset(h2load_args.split(' '), _out = process_h2load_output)
 print("h2load against envoy is done.")
+
+#run.wait()
