c700149f76b80dcb44e0d31096ab6359cc510287
Soham Sinha <sohamcodes@google.com>
2017-06-02 09:41:41 -0400

    organised to completely automate

--- init-script.sh
+++ init-script.sh
@@ -3,8 +3,8 @@ set -e
 
 sudo apt-get update
 sudo apt-get install -y make
-make lib
-make nginx
+sudo make lib
+sudo make nginx
 ./install-nghttp.sh
 mkdir -p envoy-configs
 sudo chown -R sohamcodes:sohamcodes envoy-configs
--- instance-init.sh
+++ instance-init.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# $1 - virtual-machine name
+# $2 - envoy-binary file location
+
 set -e
 
 gcloud compute instances create --zone "us-east1-b" $1 --custom-cpu 20 --custom-memory 76 --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud
+echo -e "Instance Created. Needs to wait for 30s.\n"
+sleep 30s
+gcloud config set compute/zone "us-east1-b"
+gcloud config set project "envoy-ci"
 cd ~/Google/scripts/
-gcloud compute --project "envoy-ci" scp ./Makefile ./install-nghttp.sh ./init-script.sh ./default ./nginx.conf $1:./ --zone "us-east1-b"
-cd ~/Google/envoy
-gcloud compute --project "envoy-ci" scp --recurse ./generated/configs/* $1:./envoy-configs/ --zone "us-east1-b"
-gcloud compute --project "envoy-ci" scp --recurse ./generated/configs/* $1:./envoy-configs/ --zone "us-east1-b"
-gcloud compute --project "envoy-ci" scp /tmp/envoy-docker-build/envoy/source/exe/envoy-fastbuild $1:./envoy-fastbuild --zone "us-east1-b"
+gcloud compute scp ./Makefile ./install-nghttp.sh ./init-script.sh ./default ./nginx.conf ./distribute_proc.py $1:./
+gcloud compute scp $2 $1:./envoy-fastbuild
+gcloud compute ssh --ssh-flag="-t" --command="sudo chmod +x *.sh" $1
+cd ~/Google/envoy/
+gcloud compute ssh --ssh-flag="-t" --command="sudo bash ./init-script.sh" $1
+gcloud compute scp --recurse ./generated/configs/* $1:./envoy-configs/
+gcloud compute ssh $1 --ssh-flag="-t" --command="sudo python3 distribute_proc.py ./envoy-fastbuild ./envoy-configs/simple-loopback.json"
+cd -
+gcloud compute scp $1:./result.txt ./
+echo -e "Y" | gcloud compute instances delete $1
