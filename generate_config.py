4abbd546bc5e35a7da834df3adde01164f0f84f8
Soham Sinha <sohamcodes@google.com>
2017-06-08 18:31:34 -0400

    support for template-ing of default in nginx is added

--- generate_config.py
+++ generate_config.py
@@ -16,6 +16,11 @@ def main():
   parser.add_argument("--nginx_config_filename",
                       help="the generated nginx config file name",
                       default="nginx.conf")
+  # TODO(sohamcodes): later on, there could be more than one server, so this
+  # parameter needs to be changed accordingly
+  parser.add_argument("--server_config_filename",
+                      help="the filename for the server configuration."
+                           " default: default", default="default")
   parser.add_argument("--no_of_worker_proc",
                       help="no of worker processes in nginx. default: 10",
                       type=int, default=10)
@@ -31,8 +36,15 @@ def main():
                       help="timeout for keepalive connections"
                       ". default: 250",
                       type=int, default=250)
+  parser.add_argument("--port_no",
+                      help="nginx's responsive port no"
+                      ". default: 4500",
+                      type=int, default=4500)
 
   args = parser.parse_args()
+  if args.port_no < 1024 or args.port_no > 65535:
+    parser.error("argument --port_no needs to be >= 1024 and <= 65535")
+
   j2_env = Environment(loader=FileSystemLoader(args.template_dir),
                        trim_blocks=True)
 
@@ -42,6 +54,10 @@ def main():
         worker_rlimit_nofile=str(args.worker_rlimit_nofile),
         worker_connections=str(args.worker_connections),
         keepalive_timeout=str(args.keepalive_timeout)))
+
+  with open(args.server_config_filename, "w") as f:
+    f.write(j2_env.get_template("default.template").render(
+        port_no=str(args.port_no)))
 
 if __name__ == "__main__":
   main()
