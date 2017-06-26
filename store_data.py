3e4ae68476ff3c227e91cd118c1c363a1b1a9db2
Soham Sinha <sohamcodes@google.com>
2017-06-26 10:53:46 -0400

    Storing h2load results data into a DB

---
+++ store_data.py
@@ -0,0 +1,353 @@
+"""This file creates is for the DB-related work on the statistics."""
+
+import argparse
+import json
+import re
+import subprocess
+
+import database_helpers as db_utils
+import MySQLdb
+import pexpect
+import shell_helpers as sh_utils
+
+
+class DataStoreError(Exception):
+  pass
+
+
+def CreateMutuallyExclusiveArgument(parser, argument_name, help_string):
+  """The function creates a mututally exclusive argument on parser.
+
+  It just creates an argument against the `argument_name` on parser with a
+  required help_string. The given `argument_name` is set to True and the
+  --no-`argument_name` is set to False. It doesn't set the default value. The
+  caller needs to set the default against `argument_name`.
+  Args:
+    parser: the parser on which the new argument will be created.
+    argument_name: the name of the mutually exclusive argument. The caller needs
+    to manually set the default value of the argument.
+    help_string: the required help string on the `argument_name`
+  Returns/Yields:
+    It creates a mutually exclusive argument on parser.
+  """
+  temporary_parser = parser.add_mutually_exclusive_group(required=False)
+  temporary_parser.add_argument("--{}".format(argument_name),
+                                dest=argument_name, help=help_string,
+                                action="store_true")
+
+  temporary_parser.add_argument("--no-{}".format(argument_name),
+                                dest=argument_name,
+                                action="store_false")
+
+
+def CreateTable(connection, table_name):
+  """This function creates the envoy_stat table on the give `connection`.
+
+  Args:
+    connection: the connection which will refer to a database in which the table
+    will be created.
+    table_name: the name of the table to be created.
+  Returns/Yields:
+    It creates a table on the `connection`.
+  """
+  create_table_statement = ("CREATE TABLE IF NOT EXISTS  {}"
+                            "(id INTEGER NOT NULL AUTO_INCREMENT,"
+                            "category VARCHAR(20) NOT NULL,"
+                            "total_time INTEGER UNSIGNED,"
+                            "time_for_1st_byte_max INTEGER UNSIGNED,"
+                            "time_for_1st_byte_min INTEGER UNSIGNED,"
+                            "time_for_1st_byte_mean INTEGER UNSIGNED,"
+                            "time_for_1st_byte_sd INTEGER UNSIGNED,"
+                            "time_for_1st_byte_sd_percent FLOAT UNSIGNED,"
+                            "requests_success BIGINT UNSIGNED,"
+                            "requests_started BIGINT UNSIGNED,"
+                            "requests_done BIGINT UNSIGNED,"
+                            "requests_timeout BIGINT UNSIGNED,"
+                            "requests_error BIGINT UNSIGNED,"
+                            "requests_fail BIGINT UNSIGNED,"
+                            "requests_total BIGINT UNSIGNED,"
+                            "total_data_BPS DOUBLE UNSIGNED,"
+                            "time_for_connect_max INTEGER UNSIGNED,"
+                            "time_for_connect_min INTEGER UNSIGNED,"
+                            "time_for_connect_mean INTEGER UNSIGNED,"
+                            "time_for_connect_sd INTEGER UNSIGNED,"
+                            "time_for_connect_sd_percent FLOAT UNSIGNED,"
+                            "req_per_sec_max INTEGER UNSIGNED,"
+                            "req_per_sec_min INTEGER UNSIGNED,"
+                            "req_per_sec_mean INTEGER UNSIGNED,"
+                            "req_per_sec_sd INTEGER UNSIGNED,"
+                            "req_per_sec_sd_percent FLOAT UNSIGNED,"
+                            "total_req_per_sec DOUBLE UNSIGNED,"
+                            "time_for_request_max INTEGER UNSIGNED,"
+                            "time_for_request_min INTEGER UNSIGNED,"
+                            "time_for_request_mean INTEGER UNSIGNED,"
+                            "time_for_request_sd INTEGER UNSIGNED,"
+                            "time_for_request_sd_percent FLOAT UNSIGNED,"
+                            "status_codes_2xx BIGINT UNSIGNED,"
+                            "status_codes_3xx BIGINT UNSIGNED,"
+                            "status_codes_4xx BIGINT UNSIGNED,"
+                            "status_codes_5xx BIGINT UNSIGNED,"
+                            "traffic_total_bytes BIGINT UNSIGNED,"
+                            "traffic_total_data_bytes BIGINT UNSIGNED,"
+                            "traffic_total_headers_bytes BIGINT UNSIGNED,"
+                            "traffic_total_savings FLOAT UNSIGNED,"
+                            "PRIMARY KEY (id)"
+                            ")")
+  db_utils.ExecuteAndReturnResult(connection,
+                                  create_table_statement.format(table_name))
+
+
+def GetMicrosecondData(data):
+  """The fucntion accepts a dictionary and returns microsecond data inside it.
+
+  Based on the unit (s, ms, us) inside the dictionary, the function converts to
+  microsecond value.
+  Args:
+    data: data is a dictionary of the format: {data: <val>, unit: <unit>}.
+  Returns:
+    A float value on the microsecond unit.
+  """
+  if data["unit"] == "us":
+    return float(data["data"])
+  elif data["unit"] == "ms":
+    return float(data["data"]) * 1000
+  elif data["unit"] == "s":
+    return float(data["data"]) * 1000000
+  else:
+    return None
+
+
+def GetByteData(data):
+  """The fucntion accepts a dictionary and returns Bytes value inside it.
+
+  Based on the unit (B, MB, KB), the function converts to Bytes value. It also
+  works for B/s, MB/s, etc.
+  Args:
+    data: data is a dictionary of the format: {data: <val>, unit: <unit>}.
+  Returns:
+    A float value of the Bytes unit.
+  """
+  if data["unit"].startswith("B"):
+    return float(data["data"])
+  elif data["unit"].startswith("K"):
+    return float(data["data"]) * 1000
+  elif data["unit"].startswith("M"):
+    return float(data["data"]) * 1000000
+  else:
+    return None
+
+
+def JSONToInsertCommand(table, **kwargs):
+  keys = ", ".join(kwargs.keys())
+  values = ", ".join("%s" % val for val in kwargs.values())
+  command = "INSERT INTO {} ({}) VALUES ({});".format(table, keys, values)
+  return command
+
+
+def InsertIntoTable(connection, table, json_file):
+  """The function inserts h2load result in JSON file into the provided table.
+
+  The function takes an opened JSON filestream, loads the JSON structure inside
+  it and inserts h2load statistical values into the table provided. The table
+  should have same columns, corresponding to the metrics of the h2load result,
+  written into the JSON file. It's tightly bounded with the JSON file format
+  and the table-structure.
+  Args:
+    connection: the connection with the database
+    table: name of the table in which `json_file` will be entered into
+    json_file: an opened filestream in reading mode. The file needs to be in
+    the correct JSON format, generated by the benchmarking script.
+  Returns/Yields:
+    It inserts the json_file into the `table`, by iterating the file.
+  """
+  whole_data = json.load(json_file)
+
+  # first, there will be a number of categories
+  for (key, val) in whole_data.iteritems():
+    # for each category there will be a number of benchkarking reading
+    # print key
+    for row in val:
+      # TODO(sohamcodes): We need to think of a way to do the below more
+      # gracefully, probably by keeping consistency in the JSON format and
+      # DB table column-names
+      command = JSONToInsertCommand(
+          table, category="\"{}\"".format(key),
+          total_time=GetMicrosecondData(row["total_time"]),
+          time_for_1st_byte_max=GetMicrosecondData(
+              row["time_for_1st_byte"]["max"]),
+          time_for_1st_byte_min=GetMicrosecondData(
+              row["time_for_1st_byte"]["min"]),
+          time_for_1st_byte_mean=GetMicrosecondData(
+              row["time_for_1st_byte"]["mean"]),
+          time_for_1st_byte_sd=GetMicrosecondData(
+              row["time_for_1st_byte"]["sd"]),
+          time_for_1st_byte_sd_percent=float(
+              row["time_for_1st_byte"]["sd%"]),
+          requests_success=int(row["requests_statistics"]["success"]),
+          requests_started=int(row["requests_statistics"]["started"]),
+          requests_done=int(row["requests_statistics"]["done"]),
+          requests_timeout=int(row["requests_statistics"]["timeout"]),
+          requests_error=int(row["requests_statistics"]["error"]),
+          requests_fail=int(row["requests_statistics"]["fail"]),
+          requests_total=int(row["requests_statistics"]["total"]),
+          total_data_BPS=GetByteData(row["total_data_per_sec"]),
+          time_for_connect_max=GetMicrosecondData(
+              row["time_for_connect"]["max"]),
+          time_for_connect_min=GetMicrosecondData(
+              row["time_for_connect"]["min"]),
+          time_for_connect_mean=GetMicrosecondData(
+              row["time_for_connect"]["mean"]),
+          time_for_connect_sd=GetMicrosecondData(
+              row["time_for_connect"]["sd"]),
+          time_for_connect_sd_percent=float(row["time_for_connect"]["sd%"]),
+          req_per_sec_max=float(row["req/s"]["max"]),
+          req_per_sec_min=float(row["req/s"]["min"]),
+          req_per_sec_mean=float(row["req/s"]["mean"]),
+          req_per_sec_sd=float(row["req/s"]["sd"]),
+          req_per_sec_sd_percent=float(row["req/s"]["sd%"]),
+          total_req_per_sec=float(row["total_req_per_sec"]["data"]),
+          time_for_request_max=GetMicrosecondData(
+              row["time_for_request"]["max"]),
+          time_for_request_min=GetMicrosecondData(
+              row["time_for_request"]["min"]),
+          time_for_request_mean=GetMicrosecondData(
+              row["time_for_request"]["mean"]),
+          time_for_request_sd=GetMicrosecondData(
+              row["time_for_request"]["sd"]),
+          time_for_request_sd_percent=float(row["time_for_request"]["sd%"]),
+          status_codes_2xx=int(row["status_codes_statistics"]["2xx"]),
+          status_codes_3xx=int(row["status_codes_statistics"]["3xx"]),
+          status_codes_4xx=int(row["status_codes_statistics"]["4xx"]),
+          status_codes_5xx=int(row["status_codes_statistics"]["5xx"]),
+          traffic_total_bytes=int(row["traffic_details"]["traffic_total"]),
+          traffic_total_data_bytes=int(
+              row["traffic_details"]["traffic_data"]),
+          traffic_total_headers_bytes=int(
+              row["traffic_details"]["traffic_headers"]),
+          traffic_total_savings=float(
+              row["traffic_details"]["traffic_savings%"])
+          )
+      print command
+      print db_utils.ExecuteAndReturnResult(connection, command)
+
+
+def GetInstanceIP(instance_name, project):
+  """This function gets a SQL instance's IP by running the gcloud sql describe.
+
+  Args:
+    instance_name: name of the instance
+    project: the project name in which the instance is in
+  Returns:
+    Returns a string which is the IP Address of the `instance_name`.
+  """
+  status = subprocess.check_output(
+      sh_utils.GetGcloud(["instances", "describe", instance_name],
+                         project=project, service="sql"))
+  ip = re.search(r"ipAddress:\s+([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", status)
+  return ip.group(1)
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument("--instance_name", help="the name of the gcloud instance",
+                      default="envoy-db-instance")
+  parser.add_argument("--tier", help="the tier of GCloud SQL service",
+                      default="db-n1-standard-2")
+  parser.add_argument("--username", help="username on the host DB",
+                      default="root")
+  parser.add_argument("--password",
+                      help="password for the username on the host DB",
+                      default="password")
+  parser.add_argument("--database", help="name of the database",
+                      default="envoy_stat_db")
+  parser.add_argument("--json_result", help="the JSON result file",
+                      default="result.json")
+  parser.add_argument("--table_name", help=("the table which stores "
+                                            "the benchmarking data"),
+                      default="envoy_stat")
+  parser.add_argument("--project",
+                      help="the project name in Google Cloud.",
+                      default="envoy-ci")
+  parser.add_argument("--logfile",
+                      help="the local log file for this script. New log will be"
+                           "appended to this file.", default="benchmark.log")
+  parser.add_argument("--ownip", help=("the machine's IP from where"
+                                       " the script is being run"),
+                      default="127.0.0.1")
+
+  CreateMutuallyExclusiveArgument(parser, "create_instance",
+                                  ("turn on if you want to create a "
+                                   "Google Cloud SQL instance"))
+  parser.set_defaults(create_instance=True)
+
+  CreateMutuallyExclusiveArgument(parser, "create_db",
+                                  "turn on if you want to create the DB")
+  parser.set_defaults(create_db=True)
+
+  CreateMutuallyExclusiveArgument(parser, "delete_db",
+                                  "turn on if you want to delete the DB")
+  parser.set_defaults(delete_db=True)
+
+  args = parser.parse_args()
+  logfile = open(args.logfile, "ab")
+
+  if args.create_instance:
+    sh_utils.RunGCloudService(["instances", "create", args.instance_name,
+                               "--tier", args.tier], args.project,
+                              "sql", logfile)
+    print "Google Cloud SQL Instance {} is created.".format(args.instance_name)
+  else:
+    print "Instance creation is skipped due to --no-create_instance."
+
+  sh_utils.RunGCloudService(["users", "set-password", args.username, "%",
+                             "--instance", args.instance_name, "--password",
+                             args.password], project=args.project,
+                            service="sql", logfile=logfile)
+  print "Usernames and passwords are set."
+
+  auth_ip_command = sh_utils.GetGcloud(
+      ["instances", "patch", args.instance_name,
+       "--authorized-networks", args.ownip],
+      project=args.project, service="sql")
+  auth_ip_command = " ".join(auth_ip_command)
+  pexpect.run(auth_ip_command,
+              events={"Do you want to continue (Y/n)?": "Y\n"},
+              logfile=logfile, timeout=None)
+  print ("This machine is configured to use the Google"
+         " Cloud SQL Instance {}").format(args.instance_name)
+
+  hostname = GetInstanceIP(args.instance_name, args.project)
+
+  if args.create_db:
+    connection = MySQLdb.connect(host=hostname, user=args.username,
+                                 passwd=args.password)
+    print "Connection successful."
+    db_utils.ExecuteAndReturnResult(connection, "CREATE DATABASE {};".format(
+        args.database))
+    connection.select_db(args.database)
+    print "Created DB."
+  else:
+    connection = MySQLdb.connect(host=hostname, user=args.username,
+                                 passwd=args.password, db=args.database)
+    print "Connection to DB {} is successful.".format(args.database)
+
+  # table will only be created if not exists
+  CreateTable(connection, args.table_name)
+  with open(args.json_result, "r") as f:
+    InsertIntoTable(connection, args.table_name, f)
+
+    connection.commit()
+
+  if args.delete_db:
+    db_utils.ExecuteAndReturnResult(connection,
+                                    "DROP DATABASE {};".format(args.database))
+    print "Database deletion is successful."
+  else:
+    print "Database deletion is skipped due to --no-delete_db."
+
+  connection.close()
+  print "DB Connection closed."
+
+if __name__ == "__main__":
+  main()
