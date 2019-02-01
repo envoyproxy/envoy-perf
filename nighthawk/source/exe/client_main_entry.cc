420477898480215f0343cdf3375fcfb1263cd15a
Otto van der Schaaf <oschaaf@we-amp.com>
2019-02-01 10:25:02 +0100

    Nighthawk skeleton
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- nighthawk/source/exe/client_main_entry.cc
+++ nighthawk/source/exe/client_main_entry.cc
@@ -1,21 +1,17 @@
-#include "client/client.h"
-
 #include "absl/debugging/symbolize.h"
 
 #include "nighthawk/common/exception.h"
 
 // NOLINT(namespace-nighthawk)
 
-int main(int argc, char** argv) {
+int main(int, char** argv) {
 #ifndef __APPLE__
   // absl::Symbolize mostly works without this, but this improves corner case
   // handling, such as running in a chroot jail.
   absl::InitializeSymbolizer(argv[0]);
 #endif
-  std::unique_ptr<Nighthawk::Client::Main> client;
-
   try {
-    client = std::make_unique<Nighthawk::Client::Main>(argc, argv);
+    printf("%s", "Hello world!");
   } catch (const Nighthawk::Client::NoServingException& e) {
     return EXIT_SUCCESS;
   } catch (const Nighthawk::Client::MalformedArgvException& e) {
@@ -24,5 +20,5 @@ int main(int argc, char** argv) {
     return EXIT_FAILURE;
   }
 
-  return client->run() ? EXIT_SUCCESS : EXIT_FAILURE;
+  return EXIT_SUCCESS;
 }
