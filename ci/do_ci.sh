4e7f2f69eac36565796931e6f565c87f1db27949
Otto van der Schaaf <oschaaf@we-amp.com>
2019-02-07 18:37:11 +0100

    Use clang in CI
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -13,15 +13,18 @@ function do_test() {
       //nighthawk/test:nighthawk_test
 }
 
+# TODO(oschaaf): hack, this should be done in .circleci/config.yml	# TODO(oschaaf): To avoid OOM kicking in, we throttle resources here. Revisit this later
+git submodule update --init --recursive
+
 # TODO(oschaaf): To avoid OOM kicking in, we throttle resources here. Revisit this later
 # to see how this was finally resolved in Envoy's code base. There is a TODO for when
 # when a later bazel version is deployed in CI here:
 # https://github.com/lizan/envoy/blob/2eb772ac7518c8fbf2a8c7acbc1bf89e548d9c86/ci/do_ci.sh#L86
 [ -z "$CIRCLECI" ] || export BAZEL_BUILD_OPTIONS="${BAZEL_BUILD_OPTIONS} --local_resources=4096,2,1"
 [ -z "$CIRCLECI" ] || export BAZEL_TEST_OPTIONS="${BAZEL_TEST_OPTIONS} --local_resources=4096,2,1 --local_test_jobs=4"
-
-# TODO(oschaaf): hack, this should be done in .circleci/config.yml	# TODO(oschaaf): To avoid OOM kicking in, we throttle resources here. Revisit this later
-git submodule update --init --recursive
+[ -z "$CIRCLECI" ] || export PATH=/usr/lib/llvm-7/bin:$PATH
+[ -z "$CIRCLECI" ] || export CC=clang
+[ -z "$CIRCLECI" ] || export CXX=clang++
 
 case "$1" in
   build)
