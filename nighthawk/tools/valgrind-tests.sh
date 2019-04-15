6391b43daad898770cca721de40ffd46fb64ce0a
Otto van der Schaaf <oschaaf@we-amp.com>
2019-04-15 23:59:27 +0200

    Revert "Nighthawk skeleton (#43)"
    This reverts commit bd88bf0266ee75a173c67715c461cd07ca5f43ef.
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- nighthawk/tools/valgrind-tests.sh
+++
@@ -1,4 +0,0 @@
-export TEST_WORKSPACE=.
-export TEST_SRCDIR="$(pwd)"
-export ENVOY_IP_TEST_VERSIONS="v4only"
-valgrind --leak-check=full --track-origins=yes --gen-suppressions=all --suppressions=envoy/tools/valgrind-suppressions.txt --suppressions=tools/valgrind-suppressions.txt bazel-bin/test/nighthawk_test
