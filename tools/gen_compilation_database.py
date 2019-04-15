a90b904e9786113110a43eb2f215bfd96ff2de2e
Otto van der Schaaf <oschaaf@we-amp.com>
2019-04-15 23:59:25 +0200

    Revert "Nighthawk: Statistics and updates to tooling & CI (#48)"
    This reverts commit 89caa942d407aa863bee6931f4079d0d785f8779.
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- tools/gen_compilation_database.py
+++ tools/gen_compilation_database.py
@@ -38,9 +38,6 @@ def isCompileTarget(target, args):
     if filename.startswith("google/protobuf"):
         return False
 
-    if filename.startswith("nighthawk/hdrhistogram_c"):
-        return False
-
     return True
 
 
