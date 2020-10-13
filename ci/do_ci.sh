75e98e33f8897f59d84a3a60aa7f9988c093c128
Alvin Baptiste <alvinsb@gmail.com>
2020-10-13 17:52:31 +0000

    [salvo] Update do_ci.sh to build and test salvo
    Signed-off-by: Alvin Baptiste <alvinsb@gmail.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
 
-# This is a dummy, populate with real CI.
-echo "Hello world"
+# Run a CI build/test target
+
+# Avoid using "-x" here so that we do not leak credentials
+# in build logs
+set -e
+
+# Build the salvo framework
+function build_salvo() {
+  echo "Building Salvo"
+  pushd salvo
+  bazel build //:salvo
+  popd
+}
+ 
+# Test the salvo framework
+function test_salvo() {
+  echo "Running Salvo unit tests"
+  pushd salvo
+  bazel test //test:*
+  popd
+}
+
+
+# Set the build target. If no parameters are specified
+# we default to "build"
+build_target=${1:-build}
+
+case $build_target in
+  "test")
+    test_salvo
+    ;;
+  "build")
+    build_salvo
+    ;;
+  *)
+    ;;
+esac
+
+exit 0
