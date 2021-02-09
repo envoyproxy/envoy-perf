28338c6d47ed69401ca94ea7263e782f4a13b3e6
Alvin Baptiste <11775386+abaptiste@users.noreply.github.com>
2021-02-08 16:28:25 -0800

    Add the builder module. (#98)
    This PR adds the builder module into Salvo.  In the builder module we have classes that encapsulate the steps needed to build Envoy and NightHawk from source.
    
    We have a skeleton implementation here, including the unit tests so that we maintain the test coverage level.
    
    Signed-off-by: abaptiste <abaptiste@users.noreply.github.com>

---
+++ salvo/src/lib/builder/test_base_builder.py
@@ -0,0 +1,35 @@
+import pytest
+
+from src.lib import source_manager
+from src.lib.builder import base_builder
+
+import api.control_pb2 as proto_control
+import api.source_pb2 as proto_source
+
+class DerivedBuilder(base_builder.BaseBuilder):
+  def __init__(self, manager: source_manager.SourceManager):
+    super(DerivedBuilder, self).__init__(manager)
+    pass
+
+  def do_something(self):
+    self._validate()
+
+def test_validate_must_be_overidden():
+  """Verify that the base _validate method must be overridden and raises
+  an exception otherwise.
+  """
+  control = proto_control.JobControl(remote=False, scavenging_benchmark=True)
+  control.source.add(
+      identity=proto_source.SourceRepository.SourceIdentity.SRCID_ENVOY,
+      source_path='/some_random_envoy_directory',
+  )
+  manager = source_manager.SourceManager(control)
+  builder = DerivedBuilder(manager)
+
+  with pytest.raises(NotImplementedError) as not_implemented:
+    builder.do_something()
+
+  assert str(not_implemented.value) == "Method should be overridden"
+
+if __name__ == '__main__':
+  raise SystemExit(pytest.main(['-s', '-v', __file__]))
