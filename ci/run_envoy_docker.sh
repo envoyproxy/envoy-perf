32caca0b0218ab019dacb23cb1ea9acaac04ea5d
Huang Xin <xin1.huang@intel.com>
2023-03-14 09:57:14 +0800

    Syncing configuration files from Nighthawk and Envoy (03/14/2023)
    Signed-off-by: Huang Xin <xin1.huang@intel.com>

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -35,15 +35,19 @@ else
   # in useradd below, which is need for correct Python execution in the Docker
   # environment.
   ENVOY_DOCKER_OPTIONS+=(-u root:root)
-  ENVOY_DOCKER_OPTIONS+=(-v /var/run/docker.sock:/var/run/docker.sock)
-  ENVOY_DOCKER_OPTIONS+=(--cap-add SYS_PTRACE --cap-add NET_RAW --cap-add NET_ADMIN)
+  DOCKER_USER_ARGS=()
+  DOCKER_GROUP_ARGS=()
   DEFAULT_ENVOY_DOCKER_BUILD_DIR=/tmp/envoy-docker-build
+  if [[ -n "$ENVOY_DOCKER_IN_DOCKER" ]]; then
+      ENVOY_DOCKER_OPTIONS+=(-v /var/run/docker.sock:/var/run/docker.sock)
+      DOCKER_GID="$(stat -c %g /var/run/docker.sock 2>/dev/null || stat -f %g /var/run/docker.sock)"
+      DOCKER_USER_ARGS=(--gid "${DOCKER_GID}")
+      DOCKER_GROUP_ARGS=(--gid "${DOCKER_GID}")
+  fi
   [[ -z "${BUILD_DIR_MOUNT_DEST}" ]] && BUILD_DIR_MOUNT_DEST="/build" # Salvo unique
   SOURCE_DIR="${PWD}"
   [[ -z "${SOURCE_DIR_MOUNT_DEST}" ]] && SOURCE_DIR_MOUNT_DEST="/source" # Salvo unique
-  DOCKER_GID="$(stat -c %g /var/run/docker.sock 2>/dev/null || stat -f %g /var/run/docker.sock)"
-  START_COMMAND=("/bin/bash" "-lc" "groupadd --gid ${DOCKER_GID} -f envoygroup \
-    && useradd -o --uid $(id -u) --gid ${DOCKER_GID} --no-create-home --home-dir ${BUILD_DIR_MOUNT_DEST} envoybuild `# Salvo unique`\
+  START_COMMAND=("/bin/bash" "-lc" "groupadd ${DOCKER_GROUP_ARGS[*]} -f envoygroup && useradd -o --uid $(id -u) ${DOCKER_USER_ARGS[*]} --no-create-home --home-dir ${BUILD_DIR_MOUNT_DEST} envoybuild `# Salvo unique`\
     && usermod -a -G pcap envoybuild \
     && chown envoybuild:envoygroup ${BUILD_DIR_MOUNT_DEST} `# Salvo unique`\
     && chown envoybuild /proc/self/fd/2 \
@@ -69,7 +73,7 @@ VOLUMES=(
     -v "${ENVOY_DOCKER_BUILD_DIR}":"${BUILD_DIR_MOUNT_DEST}"
     -v "${SOURCE_DIR}":"${SOURCE_DIR_MOUNT_DEST}")
 
-if ! is_windows; then
+if ! is_windows && [[ -n "$ENVOY_DOCKER_IN_DOCKER" ]]; then
     # Create a "shared" directory that has the same path in/outside the container
     # This allows the host docker engine to see artefacts using a temporary path created inside the container,
     # at the same path.
@@ -92,6 +96,7 @@ docker run --rm \
        "${VOLUMES[@]}" \
        -e AZP_BRANCH \
        -e AZP_COMMIT_SHA \
+       -e AZP_TARGET_BRANCH \
        -e HTTP_PROXY \
        -e HTTPS_PROXY \
        -e NO_PROXY \
