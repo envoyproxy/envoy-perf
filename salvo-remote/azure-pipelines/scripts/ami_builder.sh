5cd84d807e3ef60114c0186a3a9d9c342f6e8225
Jakub Sobon <mumak@google.com>
2023-03-23 17:22:56 +0000

    Build VM disk images (AMIs) from the Salvo pipeline.
    Signed-off-by: Jakub Sobon <mumak@google.com>

---
+++ salvo-remote/azure-pipelines/scripts/ami_builder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Required tokens, see:
+# https://github.com/envoyproxy/envoy-perf/blob/main/salvo-remote/azure-pipelines/README.md
+export AZURE_DEVOPS_EXT_PAT=${AZURE_DEVOPS_EXT_PAT:-}
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-}
+
+# The id of the AZP build that produced the component binaries to be included in
+# the AWS AMI.
+export BUILD_ID=${BUILD_ID:-}
+
+packer build \
+  -var "azure_devops_ext_pat=${AZURE_DEVOPS_EXT_PAT}" \
+  -var "azp_build_id=${BUILD_ID}" \
+  salvo-remote/packer-ami-build/salvo-component-vm-x64.pkr.hcl
