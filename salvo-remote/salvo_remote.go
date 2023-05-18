41800b6f5b378ac5c1a974ad173a3ebd7904d759
Jakub Sobon <jakub.sobon@elohim.sk>
2023-05-18 14:06:39 -0400

    Preheat the ASG.
    Signed-off-by: Jakub Sobon <jakub.sobon@elohim.sk>

--- salvo-remote/salvo_remote.go
+++ salvo-remote/salvo_remote.go
@@ -1,9 +1,17 @@
+// salvo-remote instruments the execution of performance tests on a sandbox
+// running remotely on AWS.
 package main
 
-import "fmt"
-import "os"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var buildID = flag.String("build_id", "", "The ID of the AZP build that produced components and binaries for this salvo-remote execution.")
 
 func main() {
-	fmt.Println("salvo-remote not implemented yet")
+	flag.Parse()
+	fmt.Printf("salvo-remote not implemented yet, executed with -build_id %q\n", *buildID)
 	os.Exit(1)
 }
