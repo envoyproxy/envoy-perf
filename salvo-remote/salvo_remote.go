3f0bb21f376b878dbd5608570172b6a7ab7d62b3
Jakub Sobon <jakub.sobon@elohim.sk>
2023-05-25 12:53:52 -0400

    Integrating Terraform into salvo-remote. (#216)
    The `salvo-remote` binary is now able to deploy a Sandbox using Terraform. Done here:
    
    - added required Go repositories into the `WORKSPACE`.
    - renamed `BUILD` to `BUILD.bazel` which is what `gazelle` uses by default.
    - moved `bazel_gazelle` dependency up to the `WORKSPACE` file as it is needed in multiple separate `.bzl` files.
    - added a patch that manually enables `cgo` for `github.com/cloudflare/circl` as per https://github.com/bazelbuild/bazel-gazelle/issues/1421.
    - added a new package `sandboxes` that manages sandbox instances.
    - validated `salvo-remote` flags and deploying a sandbox using the `sandboxes` package.
    
    Signed-off-by: Jakub Sobon <jakub.sobon@elohim.sk>

--- salvo-remote/salvo_remote.go
+++ salvo-remote/salvo_remote.go
@@ -3,16 +3,68 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/envoyproxy/envoy-perf/salvo-remote/sandboxes"
 )
 
-var buildID = flag.String("build_id", "", "The ID of the AZP build that produced components and binaries for this salvo-remote execution. Can be overridden by providing a different ID in -build_id_override.")
-var buildIDOverride = flag.String("build_id_override", "", "If set, it overrides the value set via -build_id.")
+var buildID = flag.Int("build_id", 0, "The ID of the AZP build that produced components and binaries for this salvo-remote execution. Can be overridden by providing a different ID in -build_id_override.")
+var buildIDOverride = flag.Int("build_id_override", 0, "If set, it overrides the value set via -build_id.")
+
+// validateFlags validates the provided flag values.
+func validateFlags() error {
+	if *buildID <= 0 {
+		return fmt.Errorf("got -build_id(%d), -build_id must be set to a positive integer", *buildID)
+	}
+	if *buildIDOverride < 0 {
+		return fmt.Errorf("got -build_id_override(%d), -build_id_override cannot be negative.", *buildIDOverride)
+	}
+	return nil
+}
+
+// getBuildID determines what build ID should Salvo execute with.
+func getBuildID() int {
+	if (*buildIDOverride) != 0 {
+		return *buildIDOverride
+	}
+	return *buildID
+}
+
+// sandboxManager abstracts a type that manages sandbox instances.
+type sandboxManager interface {
+	Start(context.Context, map[sandboxes.Type]sandboxes.Instances) error
+}
+
+// runSalvoRemote executes Salvo remotely and returns the exit code.
+func runSalvoRemote(sm sandboxManager) error {
+	if err := validateFlags(); err != nil {
+		return fmt.Errorf("validateFlags => %v", err)
+	}
+
+	sbxs := map[sandboxes.Type]sandboxes.Instances{
+		sandboxes.TypeDefaultSandboxX64: {getBuildID()},
+	}
+	ctx := context.Background()
+
+	if err := sm.Start(ctx, sbxs); err != nil {
+		return fmt.Errorf("sm.Start => %v", err)
+	}
+	return nil
+}
+
+// mainWithErrCode returns an error code after execution.
+func mainWithErrCode() int {
+	flag.Parse()
+	if err := runSalvoRemote(sandboxes.NewManager()); err != nil {
+		fmt.Printf("salvo-remote: %v\n", err)
+		return 1
+	}
+	return 0
+}
 
 func main() {
-	flag.Parse()
-	fmt.Printf("salvo-remote not implemented yet, executed with -build_id %q and -build_id_override %q\n", *buildID, *buildIDOverride)
-	os.Exit(1)
+	os.Exit(mainWithErrCode())
 }
---
+++ salvo-remote/sandboxes/sandboxes_test.go
@@ -0,0 +1,263 @@
+package sandboxes
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-exec/tfexec"
+)
+
+// tfexecTerraformAPI is a fake implementation of terraformAPI.
+type fakeTerraformAPI struct {
+	// initErr is the error to return when initTf is called.
+	initErr error
+	// applyErr is the error to return when Apply is called on the Terraform object.
+	applyErr error
+}
+
+// newFakeTerraformAPI creates a new fakeTerraformAPI instance.
+func newFakeTerraformAPI(initErr, applyErr error) *fakeTerraformAPI {
+	return &fakeTerraformAPI{
+		initErr:  initErr,
+		applyErr: applyErr,
+	}
+}
+
+func (f *fakeTerraformAPI) initTf(ctx context.Context, workingDir string) (terraform, error) {
+	if f.initErr != nil {
+		return nil, f.initErr
+	}
+	return newFakeTerraform(f.applyErr), nil
+}
+
+// fakeTerraform is a fake implementation of Terraform.
+type fakeTerraform struct {
+	// applyErr is the error to return when Apply is called on the Terraform object.
+	applyErr error
+}
+
+// newFakeTerraform returns a new fake Terraform object.
+func newFakeTerraform(applyErr error) *fakeTerraform {
+	return &fakeTerraform{
+		applyErr: applyErr,
+	}
+}
+
+func (f *fakeTerraform) Apply(ctx context.Context, opts ...tfexec.ApplyOption) error {
+	return f.applyErr
+}
+
+func TestStart(t *testing.T) {
+	tests := []struct {
+		desc          string
+		sbxs          map[Type]Instances
+		initErr       error
+		applyErr      error
+		wantErrSubstr string
+	}{
+		{
+			desc:          "fails when no sandboxes specified",
+			wantErrSubstr: "at least one sandbox",
+		},
+		{
+			desc: "fails on unsupported sandbox type",
+			sbxs: map[Type]Instances{
+				-1: {12345},
+			},
+			wantErrSubstr: "unsupported sandbox type",
+		},
+		{
+			desc: "fails on sandbox with no instances",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: nil,
+			},
+			wantErrSubstr: "zero instances",
+		},
+		{
+			desc: "fails with duplicate instances",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {12345, 12345},
+			},
+			wantErrSubstr: "duplicate instance",
+		},
+		{
+			desc: "fails with negative instance",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {-1},
+			},
+			wantErrSubstr: "minimum allowed",
+		},
+		{
+			desc: "fails with instance zero",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {0},
+			},
+			wantErrSubstr: "minimum allowed",
+		},
+		{
+			desc: "fails when terraform initialization fails",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {12345},
+			},
+			initErr:       errors.New("fake error"),
+			wantErrSubstr: "fake error",
+		},
+		{
+			desc: "fails when terraform apply fails",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {12345},
+			},
+			applyErr:      errors.New("fake error"),
+			wantErrSubstr: "tf.Apply =>",
+		},
+		{
+			desc: "successfully starts a sandbox instance",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {12345},
+			},
+		},
+		{
+			desc: "successfully starts multiple sandbox instances",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {12345, 67890},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			fakeAPI := newFakeTerraformAPI(tc.initErr, tc.applyErr)
+			m := NewManager(customTerraformAPI(fakeAPI))
+			ctx := context.Background()
+			err := m.Start(ctx, tc.sbxs)
+			if (err != nil) != (tc.wantErrSubstr != "") {
+				t.Errorf("Start => unexpected error: %v, wantErrSubstr: %q", err, tc.wantErrSubstr)
+			}
+			if err != nil && !strings.Contains(err.Error(), tc.wantErrSubstr) {
+				t.Errorf("Start => unexpected error substring, got:%q, want substring:%q", err, tc.wantErrSubstr)
+
+			}
+		})
+	}
+}
+
+func TestStopAll(t *testing.T) {
+	tests := []struct {
+		desc          string
+		initErr       error
+		applyErr      error
+		wantErrSubstr string
+	}{
+		{
+			desc:          "fails when terraform initialization fails",
+			initErr:       errors.New("fake error"),
+			wantErrSubstr: "fake error",
+		},
+		{
+			desc:          "fails when terraform apply fails",
+			applyErr:      errors.New("fake error"),
+			wantErrSubstr: "tf.Apply =>",
+		},
+		{
+			desc: "successfully stops all sandbox instances",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			fakeAPI := newFakeTerraformAPI(tc.initErr, tc.applyErr)
+			m := NewManager(customTerraformAPI(fakeAPI))
+			ctx := context.Background()
+			err := m.StopAll(ctx)
+			if (err != nil) != (tc.wantErrSubstr != "") {
+				t.Errorf("Stop => unexpected error: %v, wantErrSubstr: %q", err, tc.wantErrSubstr)
+			}
+			if err != nil && !strings.Contains(err.Error(), tc.wantErrSubstr) {
+				t.Errorf("Stop => unexpected error substring, got:%q, want substring:%q", err, tc.wantErrSubstr)
+
+			}
+		})
+	}
+}
+
+func TestSbxsToTfVars(t *testing.T) {
+	tests := []struct {
+		desc    string
+		sbxs    map[Type]Instances
+		want    []string
+		wantErr bool
+	}{
+		{
+			desc:    "fails when no sandboxes specified",
+			wantErr: true,
+		},
+		{
+			desc: "fails on unsupported sandbox type",
+			sbxs: map[Type]Instances{
+				-1: {12345},
+			},
+			wantErr: true,
+		},
+		{
+			desc: "fails on sandbox with no instances",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: nil,
+			},
+			wantErr: true,
+		},
+		{
+			desc: "fails with duplicate instances",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {12345, 12345},
+			},
+			wantErr: true,
+		},
+		{
+			desc: "fails with negative instance",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {-1},
+			},
+			wantErr: true,
+		},
+		{
+			desc: "fails with instance zero",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {0},
+			},
+			wantErr: true,
+		},
+		{
+			desc: "correct vars for a single instance",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {12345},
+			},
+			want: []string{
+				"default_sandbox_x64_build_ids=[12345]",
+			},
+		},
+		{
+			desc: "correct vars for multiple instances",
+			sbxs: map[Type]Instances{
+				TypeDefaultSandboxX64: {12345, 67890},
+			},
+			want: []string{
+				"default_sandbox_x64_build_ids=[12345,67890]",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := sbxsToTfVars(tc.sbxs)
+			if (err != nil) != tc.wantErr {
+				t.Errorf("sbxsToTfVars => unexpected error: %v, wantErr: %v", err, tc.wantErr)
+			}
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("sbxsToTfVars => unexpected diff (-want, +got):\n%s", diff)
+			}
+		})
+	}
+}
